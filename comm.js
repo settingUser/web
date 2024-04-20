954a139fca11d6aebbf2da7623476f2a4e99e237
samuel129028 <2625972470@qq.com>
2024-04-20 14:16:08 +0800

    1

---
+++ comm.js
@@ -0,0 +1,34 @@
+(()=>{
+const arg = getURLParameters();
+ fetch(`https://www.jfchat.cn/api/api/gano/common-url/get-weix-url?code=${arg.arg}`,{
+    method: 'GET',
+ }).then(response=>{
+if(response.ok){
+response.json().then(data=>{
+    console.log(data)
+})
+}
+
+
+ })
+
+})()
+function getURLParameters() {
+    const url = window.location.href;
+    var urlParams = {};
+    var e,
+        a = /\+/g,  // 用于替换URL中的加号
+        r = /([^&=]+)=?([^&]*)/g, // 用于匹配URL中的参数
+        d = function (s) { return decodeURIComponent(s.replace(a, " ")); }, // 用于解码URL中的参数值
+        q = url.split("?");
+    // 如果URL中有"?"，则处理其后面的参数部分
+    if (q.length > 1) {
+        e = q[1];
+        var u = e.split("&");
+        for (var i = 0; i < u.length; ++i) {
+            var j = u[i].split("=");
+            urlParams[d(j[0])] = d(j[1] || "");
+        }
+    }
+    return urlParams;
+}
